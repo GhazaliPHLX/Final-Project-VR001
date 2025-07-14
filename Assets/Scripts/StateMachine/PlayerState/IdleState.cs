9d790dcbd839c33ac9aeff974f8a9462c28dc374
Akbar Ghazali <akb.agh123@gmail.com>
2025-07-14 22:23:45 +0700

    Adding stateMachine

--- Assets/Scripts/StateMachine/PlayerState/IdleState.cs
+++ Assets/Scripts/StateMachine/PlayerState/IdleState.cs
@@ -3,23 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 using VR001;
 
-public class IdlingState : MonoBehaviour, IState
+public class IdlingState : IState
 {
-    private StateManager stateManager;
-    public IdlingState(StateManager manager)
-    {
-        this.stateManager = manager;
-    }
+    private readonly StateManager manager;
+
+    public IdlingState(StateManager m) => manager = m;
+
     public void OnEnter()
     {
+        Debug.Log("Enter Idle");
+        manager.controller.moveSpeed = 0f;
+
+        if (manager.animator != null)
+            manager.animator.SetBool("isWalking", false);
     }
+
+    public void OnUpdate() { }
 
     public void OnExit()
     {
+        Debug.Log("Exit Idle");
+        manager.animator.SetBool("isWalking", false);
     }
-
-    public void OnUpdate()
-    {
-    }
-
 }
+
+
+
